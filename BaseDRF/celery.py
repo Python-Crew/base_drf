18eec5bed5df5af84f83261324e03d68f9c9f515
MohamadAhmadi <mohammadahmadidezhnet@gmail.com>
2021-09-28 14:17:23 +0330

    celery primitive configs

--- BaseDRF/celery.py
+++ BaseDRF/celery.py
@@ -1,5 +1,5 @@
-from celery import Celery
 import os
+from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BaseDRF.settings')
