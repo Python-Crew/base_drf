f0931e79631e6a53601b1cb97010947393fa172c
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-10-22 21:48:42 +0330

    create models notification

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+class UserNotification(models.Model):
+    user = models.OneToOneField(to=User,on_delete=models.DO_NOTHING)
 
-# Create your models here.
+class Notification(models.Model):
+    user_notification = models.ForeignKey(to=UserNotification,on_delete=models.DO_NOTHING)
+    TYPE = (
+    (0, "Email"),
+    (2, "In app"),
+    (1, "SMS")
+    )
+
+    notif_type = models.CharField(max_length=10,choices=TYPE)
