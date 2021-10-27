41994a0ba615a76f6d6e3f6f26db74a3c4d2da04
tohidi <s.tohidi22@gmail.com>
2021-10-27 11:06:26 +0330

    view edit

--- payment/banks/bankfactories.py
+++ payment/banks/bankfactories.py
@@ -1,6 +1,4 @@
-from __future__ import absolute_import, unicode_literals
 import importlib
-import logging
 from .banks import BaseBank
 from BaseDRF.settings import BANK_CLASS
 
@@ -11,22 +9,11 @@ class BankFactory:
 
     def _import_bank(self, bank_class: str):
         bank = self._banks[bank_class]
-        print(bank)
         package, attr = bank.rsplit(".", 1)
-        print(package)
-        print(attr)
         bank = getattr(importlib.import_module(package), bank_class)
-
-        print(bank)
-        logging.debug("Import bank class")
-
         return bank
 
     def create(self, bank_class: str) -> BaseBank:
-        """Build bank class"""
-
         bank_klass = self._import_bank(bank_class)
         bank = bank_klass()
-
-        logging.debug("Create bank")
         return bank
