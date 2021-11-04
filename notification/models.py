f2b324a9d51fe0f10fa4b3ed5c15e0390794014e
arezoo <arezoo.darvish6969@gmail.com>
2021-11-04 13:12:49 +0330

    change types

--- notification/models.py
+++ notification/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 class UserNotification(models.Model):
-    user = models.OneToOneField(to=User,on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(to=User,on_delete=models.CASCADE)
 
 class Notification(models.Model):
-    user_notification = models.ForeignKey(to=UserNotification,on_delete=models.DO_NOTHING)
-    TYPE = (
-    (0, "Email"),
-    (2, "In app"),
-    (1, "SMS")
+    user_notification = models.ForeignKey(to=UserNotification,on_delete=models.CASCADE)
+    TYPE_SMS = 'sms'
+    TYPE_EMAIL = 'email'
+    TYPE_IN_APP = 'In app'
+
+    TYPES = (
+        (TYPE_SMS, 'sms'),
+        (TYPE_EMAIL, 'email'),
+        (TYPE_IN_APP, 'In app'),
     )
 
-    notif_type = models.CharField(max_length=10,choices=TYPE)
+    notif_type = models.CharField(max_length=10,choices=TYPES)
