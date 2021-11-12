ad8a2b6593da0cc6af9e0ce67ec5b4205a3a8caf
arezoo <arezoo.darvish6969@gmail.com>
2021-11-12 15:09:40 +0330

    change way of importing notification app

--- BaseDRF/settings.py
+++ BaseDRF/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notification'
+    'notification.apps.NotificationConfig'
 ]
 
 MIDDLEWARE = [
